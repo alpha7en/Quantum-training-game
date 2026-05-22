c6cb338665f61c1a21ebafb491506b18d278eaae
alpha7en <chuklanov.pavel@gmail.com>
2026-05-22 18:26:07 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+// Self-cleaning service worker: immediately unregisters itself and deletes all cache storages.
+// This resolves the deadlock where a cached broken version of the app prevents updates.
+
 self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
